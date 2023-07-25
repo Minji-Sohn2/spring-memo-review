9ce72b6eff28e0d95042b0d77e5620784c581c0e
Minji-Sohn2 <cyzh1311@gmail.com>
2023-07-25 16:04:33 +0900

    :tada: second commit(first commit 때 빠뜨린 것

---
+++ src/main/java/com/example/springmemoreview/SpringMemoReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmemoreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMemoReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMemoReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springmemoreview/SpringMemoReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmemoreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMemoReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
